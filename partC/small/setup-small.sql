6661c03f65b4387be1999af8a83965433221a2b2
ddang8-jpeg <70651977+ddang8-jpeg@users.noreply.github.com>
2022-04-14 21:36:12 -0400

    untested full setup-small.sql build

--- partC/small/setup-small.sql
+++ partC/small/setup-small.sql
@@ -31,6 +31,77 @@ CREATE TABLE Species (
   PRIMARY KEY(Scientific_Name)
 );
 
+CREATE TABLE States (
+  Name                  VARCHAR(30),
+  State_Code            VARCHAR(2),
+  Esa_Listing_Status    VARCHAR(40),
+  Region                VARCHAR(10),
+  Division              VARCHAR(40),
+  PRIMARY KEY(State_Name)
+);
+
+CREATE TABLE CurrentRange (
+  Scientific_Name       VARCHAR(40),
+  State_Code            VARCHAR(2),
+  FOREIGN KEY(Scientific_Name) ON DELETE CASCADE ON UPDATE CASCADE,
+  FOREIGN KEY(State_Code)
+);
+
+CREATE TABLE Conservation (
+  Plan_ID               VARCHAR(10),
+  Plan_Title            VARCHAR(40),
+  Plan_Type             VARCHAR(3),
+  PRIMARY KEY(Plan_ID)
+);
+
+CREATE TABLE CurrentRange (
+  Scientific_Name       VARCHAR(40),
+  Plan_ID               VARCHAR(10),
+  FOREIGN KEY(Scientific_Name) ON DELETE CASCADE ON UPDATE CASCADE,
+  FOREIGN KEY(Plan_ID) ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+CREATE TABLE Refuge (
+  Refuge_ID             VARCHAR(10),
+  Refuge_Name           VARCHAR(40),
+  PRIMARY KEY(Plan_ID)
+);
+
+CREATE TABLE Refuges (
+  Scientific_Name       VARCHAR(40),
+  Refuge_ID             VARCHAR(10),
+  FOREIGN KEY(Scientific_Name) ON DELETE CASCADE ON UPDATE CASCADE,
+  FOREIGN KEY(Refuge_ID) ON DELETE CASCADE ON UPDATE CASCADE
+);
+
 LOAD DATA LOCAL INFILE '/home/ddang8/DB_sp22/DB_Project/partC/small/Species-small.csv'
 INTO TABLE Species
+FIELDS TERMINATED BY ',';
+
+LOAD DATA LOCAL INFILE '/home/ddang8/DB_sp22/DB_Project/partC/small/Species-DelistedSpecies-small.csv'
+INTO TABLE DelistedSpecies
+FIELDS TERMINATED BY ',';
+
+LOAD DATA LOCAL INFILE '/home/ddang8/DB_sp22/DB_Project/partC/small/State-small.csv'
+INTO TABLE States
+FIELDS TERMINATED BY ',';
+
+LOAD DATA LOCAL INFILE '/home/ddang8/DB_sp22/DB_Project/partC/small/CurrentRange-small.csv'
+INTO TABLE CurrentRange
+FIELDS TERMINATED BY ',';
+
+LOAD DATA LOCAL INFILE '/home/ddang8/DB_sp22/DB_Project/partC/small/Conservation-small.csv'
+INTO TABLE Conservation
+FIELDS TERMINATED BY ',';
+
+LOAD DATA LOCAL INFILE '/home/ddang8/DB_sp22/DB_Project/partC/small/Planned-small.csv'
+INTO TABLE Planned
+FIELDS TERMINATED BY ',';
+
+LOAD DATA LOCAL INFILE '/home/ddang8/DB_sp22/DB_Project/partC/small/Refuge-small.csv'
+INTO TABLE Refuge
+FIELDS TERMINATED BY ',';
+
+LOAD DATA LOCAL INFILE '/home/ddang8/DB_sp22/DB_Project/partC/small/Refuges-small.csv'
+INTO TABLE Refuges
 FIELDS TERMINATED BY ',';
