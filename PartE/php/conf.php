3cf1f99a594113e6428836197d7fb6e4c8523c30
Derek <ddang8@jhu.edu>
2022-05-09 22:39:46 -0400

    moved php to seperate file

---
+++ PartE/php/conf.php
@@ -0,0 +1,9 @@
+
+<?php
+	//these are credentials which will be used by open.php to create connection to dbase
+	//make updates here as noted below, and save this file as conf.php
+	$dbhost = "dbase.cs.jhu.edu"; // database host
+	$dbuser = "22sp_ddang8";    // username on db host: replace 22sp_jhopkin1 with yours
+	$dbpass = "iQGa6Y2x0F";            // password on db host: replace ***** with yours
+	$dbname = "22sp_ddang8_db"; // the db's name: replace 22sp_jhopkin1_db with yours
+?>
